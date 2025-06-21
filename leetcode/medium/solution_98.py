ec7eb4097a3543d3b876201ca14001dbbb017f3c
Jackson <56964585@qq.com>
2025-06-21 12:06:27 +0800

    添加力扣热题100-43

---
+++ leetcode/medium/solution_98.py
@@ -0,0 +1,31 @@
+"""
+验证二叉搜索树
+给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+有效 二叉搜索树定义如下：
+节点的左子树只包含 小于 当前节点的数。
+节点的右子树只包含 大于 当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1：
+输入：root = [2,1,3]
+输出：true
+示例 2：
+输入：root = [5,1,4,null,null,3,6]
+输出：false
+解释：根节点的值是 5 ，但是右子节点的值是 4 。
+提示：
+树中节点数目范围在[1, 104] 内
+-231 <= Node.val <= 231 - 1
+"""
+from math import inf
+from typing import Optional
+
+from treenode import TreeNode
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode], lower: float = -inf, upper: float = inf) -> bool:
+        if root is None:
+            return True
+        if root.val <= lower or root.val >= upper:
+            return False
+        return self.isValidBST(root.left, lower, root.val) and self.isValidBST(root.right, root.val, upper)
